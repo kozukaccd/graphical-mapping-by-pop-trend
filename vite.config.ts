01b061dd85a84359aeb651cb17c2713eb6820adc
tomakozuka <ichiba_k@kezukaya.com>
2022-08-16 22:19:20 +0900

    WSL上でviteのHMRが効かない問題を回避

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,11 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // WSL上のコンテナで起動するとViteのHMRが効かない問題があるためこれで回避する
+  server: {
+    open: true,
+    watch: {
+      usePolling: true,
+    },
+  },
 });
