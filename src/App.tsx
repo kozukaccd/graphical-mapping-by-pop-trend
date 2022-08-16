015d447f99b7739cae1d77c721403f94b1e73397
tomakozuka <ichiba_k@kezukaya.com>
2022-08-17 00:35:09 +0900

    StyledComponent導入

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import "./App.css";
 import { TestComponent } from "./TestComponent";
 
 const App: React.FC = () => {
--- src/TestComponent.tsx
+++ src/TestComponent.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-
+import styled from "styled-components";
 interface Props {
   title: string;
 }
@@ -8,6 +8,13 @@ export const TestComponent: React.FC<Props> = ({ title }) => {
   return (
     <div>
       <h1>{title}</h1>
+      <TestStyledComponent>
+        <p>Content Content Content</p>
+      </TestStyledComponent>
     </div>
   );
 };
+
+const TestStyledComponent = styled.div`
+  color: red;
+`;
