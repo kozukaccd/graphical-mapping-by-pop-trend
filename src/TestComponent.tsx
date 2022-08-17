4bc181a5ec22c354e976ad5b16d2b8cbad2b975a
tomakozuka <ichiba_k@kezukaya.com>
2022-08-17 19:47:39 +0900

    Recharts.js導入

--- src/TestComponent.tsx
+++ src/TestComponent.tsx
@@ -1,21 +1,99 @@
 import React from "react";
 import styled from "styled-components";
+import {
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+} from "recharts";
 interface Props {
   title: string;
 }
+const data = [
+  {
+    name: "Page A",
+    uv: 4000,
+    pv: 2400,
+    amt: 2400,
+  },
+  {
+    name: "Page B",
+    uv: 3000,
+    pv: 1398,
+    amt: 2210,
+  },
+  {
+    name: "Page C",
+    uv: 2000,
+    pv: 9800,
+    amt: 2290,
+  },
+  {
+    name: "Page D",
+    uv: 2780,
+    pv: 3908,
+    amt: 2000,
+  },
+  {
+    name: "Page E",
+    uv: 1890,
+    pv: 4800,
+    amt: 2181,
+  },
+  {
+    name: "Page F",
+    uv: 2390,
+    pv: 3800,
+    amt: 2500,
+  },
+  {
+    name: "Page G",
+    uv: 3490,
+    pv: 4300,
+    amt: 2100,
+  },
+];
 
 export const TestComponent: React.FC<Props> = ({ title }) => {
-  const apikey = process.env.VITE_API_KEY;
+  // const apikey = process.env.VITE_API_KEY;
   return (
     <div>
       <h1>{title}</h1>
       <TestStyledComponent>
-        <p>{apikey}</p>
+        <LineChart
+          width={1000}
+          height={800}
+          data={data}
+          margin={{
+            top: 5,
+            right: 30,
+            left: 20,
+            bottom: 5,
+          }}
+        >
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="name" />
+          <YAxis />
+          <Tooltip />
+          <Legend />
+          <Line
+            type="monotone"
+            dataKey="pv"
+            stroke="#8884d8"
+            activeDot={{ r: 8 }}
+          />
+          <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
+        </LineChart>
       </TestStyledComponent>
     </div>
   );
 };
 
 const TestStyledComponent = styled.div`
-  color: red;
+  width: 50%;
+  margin: 0 auto;
+  margin-top: 150px;
 `;
