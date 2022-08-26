c9cd345d7ef7d933efe81223a6f6ac171c6eb67f
tomakozuka <ichiba_k@kezukaya.com>
2022-08-26 20:55:44 +0900

    ~でsrc内を参照するようエイリアス設定

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import EnvironmentPlugin from "vite-plugin-environment";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), EnvironmentPlugin("all")],
+  plugins: [react(), EnvironmentPlugin("all"), tsconfigPaths()],
   // WSL上のコンテナで起動するとViteのHMRが効かない問題があるためこれで回避する
   server: {
     open: true,
